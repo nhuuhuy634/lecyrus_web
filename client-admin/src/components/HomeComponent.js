ddb93facda41f0b7e6ce74e1d49f7a54a46dfbe1
nhuuhuy634 <daxua1234567@gmail.com>
2024-05-16 17:49:29 +0700

    initial web

---
+++ client-admin/src/components/HomeComponent.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+    render() {
+        return (
+            <div className="align-center">
+                <h2 className="text-center">ADMIN HOME</h2>
+                <img src="https://media.giphy.com/media/uqMQsE4HKYvfnjhIRQ/giphy.gif?cid=ecf05e470a5jjomxqyinv4ifup1d2vej46s99fzjojhmshrt&ep=v1_gifs_search&rid=giphy.gif&ct=g" width="800px" height="600px" alt="" />
+            </div>
+        );
+    }
+}
+export default Home;
